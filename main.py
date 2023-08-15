3d9d210ff9472b3ddcc7a399701357629bf68c1a
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-08-15 14:24:55 -0500

    Primera versión de prueba de Selenium

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print('Hello World!')
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
+# Si ChromeDriver no está en tu PATH, especifica su ubicación directamente
+# driver = webdriver.Chrome(executable_path='/ruta/del/chromedriver')
+driver = webdriver.Chrome()
 
-if __name__ == '__main__':
-    main()
+# Navega hacia Google
+driver.get("https://www.google.com")
+
+# Encuentra la barra de búsqueda y escribe la consulta
+search_box = driver.find_element_by_name("q")
+search_box.send_keys("¿Es Google hoy día, después de la aparición de ChatGPT, las nuevas páginas amarillas?")
+search_box.send_keys(Keys.RETURN)  # Simula presionar "Enter"
+
+# Esperar un poco (esto es solo para que puedas ver el resultado; en producción, podrías omitir o reducir esta espera)
+time.sleep(5)
+time.sleep(60)
+
+# Cierra el navegador
+driver.quit()
