bc6dd76bd7c71a837e159b401f43de27f170abb6
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-08-15 14:31:39 -0500

    Ajuste para usar Selenium 4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
 # Si ChromeDriver no está en tu PATH, especifica su ubicación directamente
@@ -10,13 +11,9 @@ driver = webdriver.Chrome()
 driver.get("https://www.google.com")
 
 # Encuentra la barra de búsqueda y escribe la consulta
-search_box = driver.find_element_by_name("q")
+search_box = driver.find_element(By.NAME, "q")
 search_box.send_keys("¿Es Google hoy día, después de la aparición de ChatGPT, las nuevas páginas amarillas?")
 search_box.send_keys(Keys.RETURN)  # Simula presionar "Enter"
 
-# Esperar un poco (esto es solo para que puedas ver el resultado; en producción, podrías omitir o reducir esta espera)
-time.sleep(5)
-time.sleep(60)
-
-# Cierra el navegador
-driver.quit()
+# Esperar un poco
+time.sleep(25)
