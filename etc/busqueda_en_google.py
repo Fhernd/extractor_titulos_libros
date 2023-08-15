b496d05cbe7ae16ce6b6f7e4ca98cf5f8501b5b5
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-08-15 14:34:07 -0500

    Búsqueda en buscador Google

---
+++ etc/busqueda_en_google.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+# Si ChromeDriver no está en tu PATH, especifica su ubicación directamente
+# driver = webdriver.Chrome(executable_path='/ruta/del/chromedriver')
+driver = webdriver.Chrome()
+
+# Navega hacia Google
+driver.get("https://www.google.com")
+
+# Encuentra la barra de búsqueda y escribe la consulta
+search_box = driver.find_element(By.NAME, "q")
+search_box.send_keys("¿Es Google hoy día, después de la aparición de ChatGPT, las nuevas páginas amarillas?")
+search_box.send_keys(Keys.RETURN)  # Simula presionar "Enter"
+
+time.sleep(3)  # Esperar un poco
+
+driver.maximize_window()
+
+# Esperar un poco
+time.sleep(25)
