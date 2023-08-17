b7c5938f8efdf44db1188324db6da8a2d7e3e5ba
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-08-17 15:14:15 -0500

    Función para realizar un prompt en ChatGPT

--- main.py
+++ main.py
@@ -101,6 +101,54 @@ def login(driver, email, password):
     btn_next = driver.find_element(By.CSS_SELECTOR, "button.-my-1")
     btn_next.click()
 
+
+def hacer_prompt(driver, descripcion):
+    """
+    Hace un prompt en OpenAI.
+
+    Args:
+        driver (selenium.webdriver.chrome.webdriver.WebDriver): Driver de Selenium.
+        descripcion (str): Descripción del prompt.
+    """
+    
+    # Encontrar el textarea con el ID "prompt-textarea":
+    prompt_textarea = driver.find_element(By.ID, "prompt-textarea")
+
+    # Escribir el siguiente texto:
+    prompt = f"""En el siguiente texto encuentra los títulos de libros: $$$ {descripcion} $$$ El formato de la salida debe ser el siguiente: Autor - Título del libro Por ejemplo: 1. Immanuel Kant - Crítica de la razón pura 2. Immanuel Kant - Crítica de la razón práctica 3. Immanuel Kant - Crítica del juicio
+    """
+
+    prompt_textarea.send_keys(prompt)
+
+    # Presionar la tecla Return:
+    prompt_textarea.send_keys(Keys.RETURN)
+
+    time.sleep(5)
+
+    # Verificar si existe un elemento con la clase "text-2xl":
+    while True:
+        try:
+            driver.find_element(By.CLASS_NAME, "text-2xl")
+        except:
+            break
+
+    try:
+        # Extraer el último elemento que tiene las siguientes clases "markdown prose w-full break-words dark:prose-invert light":
+        output = driver.find_elements(By.CSS_SELECTOR, "div.markdown.prose.w-full.break-words.dark\:prose-invert.light")[-1]
+
+        if output:
+            # Extrae el texto de cada uno de los elementos li que hay ahí dentro:
+            libros = []
+            for li in output.find_elements(By.TAG_NAME, "li"):
+                print('libro: ', li.text)
+                libros.append(li.text)
+        else:
+            return []
+    except Exception as e:
+        print('Error: ', e)
+        return []
+
+
 EMAIL, PASSWORD = obtener_credenciales()
 
 driver = generar_driver()
@@ -108,34 +156,5 @@ driver = generar_driver()
 time.sleep(1)
 
 login(driver, EMAIL, PASSWORD)
-
-# Encontrar el textarea con el ID "prompt-textarea":
-prompt_textarea = driver.find_element(By.ID, "prompt-textarea")
-
-# Escribir el siguiente texto:
-prompt = """En el siguiente texto encuentra los títulos de libros: $$$ FdB 3x08 | Cerramos los episodios dedicados al contexto y la vida de Immanuel Kant con la última etapa de su vida. Es precisamente a partir de los años 80 cuando verán la luz sus trabajos más influyentes. Nos encontramos entonces con un hombre sencillo pero que detestaba las servidumbres, y que tuvo que afrontar serias advertencias por la publicación de La religión dentro de los límites de la mera razón (1793). Celebramos, pues, el Día de la Filosofía con un homenaje a un filósofo que dignificó la actividad con su legado.   "Un libro en el bolsillo" lo dedicamos al último ensayo de Miquel Seguró: Vulnerabilidad, un texto que aborda su dimensión íntima y colectiva (el Pathos y  el Ethos) a partir de la vivencia intelectual de René Descartes y su duda como experiencia metafísica. ❗  FILOSOFÍA DE BOLSILLO sólo es y será posible gracias a ti. Hazte mecenas en https://www.filosofiadebolsillo.com/patreon y accede a este y otros EPISODIOS COMPLETOS y a tus recompensas. Si quieres apoyar el proyecto y tienes cualquier duda, escribe a correo@filosofiadebolsillo.com  ---   Send in a voice message: https://podcasters.spotify.com/pod/show/diego-civilotti/message $$$ El formato de la salida debe ser el siguiente: Autor - Título del libro Por ejemplo: 1. Immanuel Kant - Crítica de la razón pura 2. Immanuel Kant - Crítica de la razón práctica 3. Immanuel Kant - Crítica del juicio
-"""
-
-prompt_textarea.send_keys(prompt)
-
-# Presionar la tecla Return:
-prompt_textarea.send_keys(Keys.RETURN)
-
-time.sleep(5)
-
-# Verificar si existe un elemento con la clase "text-2xl":
-while True:
-    try:
-        driver.find_element(By.CLASS_NAME, "text-2xl")
-    except:
-        break
-
-# Extraer el último elemento que tiene las siguientes clases "markdown prose w-full break-words dark:prose-invert light":
-output = driver.find_elements(By.CSS_SELECTOR, "div.markdown.prose.w-full.break-words.dark\:prose-invert.light")[-1]
-
-if output:
-    # Extrae el texto de cada uno de los elementos li que hay ahí dentro:
-    for li in output.find_elements(By.TAG_NAME, "li"):
-        print('libro: ', li.text)
 
 time.sleep(1000)
